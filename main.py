dfe626b45ba9a4077b1e919436f8b80dbd1fb1a5
Param Chhabra <paramchhabra81@gmail.com>
2026-09-13 15:36:58 +0530

    folder creation

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from Factline.pipeline.pipeline import Pipeline
 from Factline.utils.helper import read_config, read_state, write_state
+import os
 
 
 def main():
+    os.makedirs("artifacts/audio", exist_ok=True)
+    os.makedirs("artifacts/images", exist_ok=True)
+    os.makedirs("artifacts/videos", exist_ok=True)
+    os.makedirs("logs/", exist_ok=True)
 
     config = read_config()
     state = read_state()
