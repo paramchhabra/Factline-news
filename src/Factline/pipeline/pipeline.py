de1772cb30a50fafd1f4b03ef601ff621f8e2c5f
Param Chhabra <paramchhabra81@gmail.com>
2026-09-21 08:47:46 +0530

    limit to script writing

--- src/Factline/pipeline/pipeline.py
+++ src/Factline/pipeline/pipeline.py
@@ -120,7 +120,7 @@ class Pipeline:
         script_config = ConfigBox({
             "topic": topic,
             "language": language,
-            "transcript": news_data
+            "transcript": news_data[:22000]
         })
 
         script_component = ScriptGen(
